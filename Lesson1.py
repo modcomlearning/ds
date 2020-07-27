983e7ba7dfd9f605618cdf171fb6b2febec65df3
modcomlearnig <modcomlearnig@gmail.com>
2020-07-27 15:55:17 +0300

    Third

---
+++ Lesson1.py
@@ -0,0 +1,33 @@
+
+number = 10  # 'number'  is the name of the variable, 10 is the value
+print(number)
+
+# 'identifier' the name of the variable
+age  = 20  # number - int
+money = 400.6   # number - float
+name = "Joseph"   # string
+
+# int, float, strings are called data types
+print(age)
+print(money)
+print(name)
+
+# naming rules for variable names/identifier
+# the name should be meaningful of your choice
+myMoney = 700
+myGrade = 'A'
+PI = 3.14  # constants use capital
+my_age = 10
+
+# No spacing
+myCash = 7000
+
+print(myMoney)
+
+
+
+
+
+
+
+
---
+++ Lesson1b.py
@@ -0,0 +1,25 @@
+
+# simple calculator
+# create 3 variables to hold the 3 numbers
+# print the 3 variables
+# add the 3 numbers
+# print the answer
+
+num1 = float(input('What is your 1st number?'))  # float
+num2 = int(input('What is your 2nd number?'))    # int
+num3 = int(input('What is your 3rd number?'))    # int
+myName = str(input('What is your name?'))  # string
+
+print('Your first number: ', num1) # concatenation
+print('Your second number: ', num2)
+print('Your last number is: ', num3)
+print('Your names is: ', myName)
+
+# additions/multiply/division
+# Operators
+total = num1 + num2 + num3
+print('Your answer is: ', total)
+
+product = num1 * num2 * num3
+print('The multiplication is: ', product)
+
