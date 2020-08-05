8281387b5d1dd7d57bb60a60fcbf9c6c14708430
modcomlearnig <modcomlearnig@gmail.com>
2020-08-05 12:32:40 +0300

    Fifth Commit

--- Lesson8b.py
+++ Lesson8b.py
@@ -10,19 +10,28 @@ class Account:
         self.balance = balance
 
     def deposit(self, amount_deposited):
-        new_bal = amount_deposited + self.balance  # add deposited to existing bal in self
+        self.balance = amount_deposited + self.balance  # add deposited to existing bal in self
         charges = 5
-        print('You new balance is ', new_bal - charges)
+        print('You new balance is after deposit: ', self.balance - charges)
         # sms code
         print('SMS sent to ', self.phone)
         # you'll try withdraw.
 
 
+    def withdraw(self, withdrawn_amount):
+        self.balance = self.balance - withdrawn_amount
+        # more logic here
+        print('You withdrew ', withdrawn_amount)
+        print('Your balance is ', self.balance)
+
 # create an object from Account class
-object =  Account('Jose',45454,22,555555,100,300)   # 100 is fee, 300 is default balance
+x =  Account('Jose',45454,22,729225710,100,300)   # 100 is fee, 300 is default balance
 # use above object to access its behv . i.e deposit
-object.deposit(amount_deposited=2000)
+x.deposit(amount_deposited=2000)
+x.withdraw(withdrawn_amount=3000)
 # dot-notation above means you are accessing object behv
+# Object should function, they should have functions
+print(type(x))
 
 
 
